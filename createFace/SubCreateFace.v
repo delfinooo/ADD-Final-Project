976c9c5935ed08891b38001fdccb15c50640420e
tylrngyn <tnguyent037@gmail.com>
2025-04-22 18:55:51 -0500

    Update SubCreateFace.v
    I update description

--- createFace/SubCreateFace.v
+++ createFace/SubCreateFace.v
@@ -1,7 +1,13 @@
 // ECE 5440 10409
 // Tyler Nguyen
 // SubCreateFace Module
-// Description
+// This module controls the display of facial expressions on a 7-segment display
+// based on system events such as achieving a new high score or player death.
+// Depending on the input signals (`newHighScore`, `died`), it sets the appropriate 
+// eye and mouth patterns to represent a happy or sad face and enables a timer to 
+// control how long the face is shown. When the timer expires (`timeout`), the face 
+// is cleared. The `showFace` and `timer_enable` outputs control when the face should
+// be displayed and when the timer should be running.
 
 module SubCreateFace(newHighScore, died, timeout, timer_enable, eyes, mouth, showFace, clk, rst);
     input clk, rst;
