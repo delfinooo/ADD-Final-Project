e3468adbcc8d4c2a8f35a5b301e919e71b879fab
tylrngyn <tnguyent037@gmail.com>
2025-04-22 18:57:40 -0500

    Update CreateFace.v

--- createFace/CreateFace.v
+++ createFace/CreateFace.v
@@ -1,7 +1,13 @@
 // ECE 5440 10409
 // Tyler Nguyen
 // CreateFace Module
-// Description
+// This top-level module manages the display of facial expressions based on system events 
+// such as achieving a new high score or dying. It instantiates the `SubCreateFace` module 
+// to determine which face to show (happy or sad) and uses a timer (`OneSecTimer`) to control 
+// how long the face remains visible. The timer is enabled when a face is displayed and triggers 
+// a timeout signal to clear the face after one second. Outputs include the 7-segment display 
+// patterns for eyes and mouth, as well as a control signal to indicate whether the face should 
+// currently be shown.
 
 module CreateFace(newHighScore, died, eyes, mouth, showFace, clk, rst);
     input clk, rst;
